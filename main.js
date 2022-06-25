f379eb2e469c1bfe6aab89b153756bb1b27a1914
mmvanheusden <50550545+mmvanheusden@users.noreply.github.com>
2022-06-25 20:52:34 +0200

    Check if dotnet is installed before downloading

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    resizable: true,
-    width: 400,
-    height: 520,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+        //resizable: false,
+        width: 425,
+        height: 575,
+        maximizable: false, // lol this does not work on kde plasma
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+    //TODO: don't do this in the release lol
+    mainWindow.webContents.openDevTools({mode: 'detach'})
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
-
-// PUT EVERYTHING ELSE BELOW HERE
-/*
-let spawn = require("child_process").spawn;
-
-let test123 = spawn("konsole")
-*/
