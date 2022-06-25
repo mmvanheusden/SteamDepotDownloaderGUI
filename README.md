3cdb8fe138893a782b6bda26ba087c0e0215aeeb
mmvanheusden <50550545+mmvanheusden@users.noreply.github.com>
2022-06-25 09:16:01 +0200

    Add a README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SteamDepotDownloaderGUI Electron rewrite (experimental)
+
+
+## How to setup dev environment
+* First, install `npm` and `nodejs` on your system.
+
+> For Arch Linux users, run `sudo pacman -S npm nodejs`
+
+* Then, git clone this repo, change direcory into the repo and run `git checkout rewrite`.
+* Now run `npm install`
+* You now can start the GUI with `npm start`
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const { app, BrowserWindow } = require('electron')
-// TODO: this: https://manu.ninja/simple-electron-gui-wrapper-for-a-command-line-utility/
-const createWindow = () => {
-    const win = new BrowserWindow({
-      autoHideMenuBar: true,
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-}
-app.whenReady().then(() => {
-    createWindow()
+const path = require('path')
 
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
