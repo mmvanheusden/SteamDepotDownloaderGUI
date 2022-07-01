4759a418c6a0045198490df33f3f051e8a6c7822
mmvanheusden <ferughiueryhgoeher@gmail.com>
2022-07-01 19:17:20 +0200

    Revert "Add support for windows (experimental)"
    This reverts commit f77477b52eec14ed57f50af6a36ee85f169a9987.

--- downloader.js
+++ downloader.js
@@ -53,94 +53,40 @@ function submitForm() {
 	}
 	if (os.platform().includes("linux")) {
 		console.info("Using Linux")
-	}
-	which("dotnet", function (er) {
-		// er is returned if no "node" is found on the PATH
-		// if it is found, then the absolute path to the exec is returned
-		if (er) {
-			console.warn("dotnet not found(in system path)")
-			document.getElementById("alert").hidden = false
-			document.getElementById("alertbtn").hidden = false
-			document.getElementById("download").disabled = true
+		which("dotnet", function (er) {
+			// er is returned if no "node" is found on the PATH
+			// if it is found, then the absolute path to the exec is returned
+			if (er) {
+				console.warn("dotnet not found(in system path)")
+				document.getElementById("alert").hidden = false
+				document.getElementById("alertbtn").hidden = false
+				document.getElementById("download").disabled = true
 
-		} else {
-			console.log("Found dotnet in system path")
-			file.on("finish", function () {
-				file.close()
-				const {exec} = require("child_process")
-				console.info("Extracting DepotDownloader binary")
+			} else {
+				console.log("Found dotnet in system path")
+				try {
+					// first check if directory already exists
+					if (!fs.existsSync("./depotdownloader")) {
+						fs.mkdirSync("./depotdownloader")
+						console.info("Directory is created.")
+					} else {
+						console.info("Directory already exists.")
+					}
+				} catch (err) {
+					console.error(err)
+				}
+				const file = fs.createWriteStream("./depotdownloader/depotdownloader.zip")
+				const request = https.get("https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.4.6/depotdownloader-2.4.6.zip", function (response) {
+					response.pipe(file)
+					console.info("Downloaded DepotDownloader binary zip")
+				})
+				file.on("finish", function () {
+					file.close()
+					const {exec} = require("child_process")
+					console.info("Extracting DepotDownloader binary")
 
-				// The following code has delays in between because async io operations won't work well
-				// Cleans up old files (but not the downloaded zip)
-				if (os.platform().includes("win")) {
-					setTimeout(() => {
-						exec("rmdir ./depotdownloader/", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.error(stderr)
-							}
-						})
-						console.debug("Removed leftover files")
-						try {
-							// first check if directory already exists
-							if (!fs.existsSync("./depotdownloader")) {
-								fs.mkdirSync("./depotdownloader")
-								console.info("Directory is created.")
-							} else {
-								console.info("Directory already exists.")
-							}
-						} catch (err) {
-							console.error(err)
-						}
-						const file = fs.createWriteStream("./depotdownloader/depotdownloader.zip")
-						const request = https.get("https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.4.6/depotdownloader-2.4.6.zip", function (response) {
-							response.pipe(file)
-							console.info("Downloaded DepotDownloader binary zip")
-						})
-					}, 100)
-					// Unzips downloaded zip
-					setTimeout(() => {
-						exec("tar -xf ./depotdownloader/depotdownloader.zip -c ./depotdownloader/", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.error(stderr)
-							}
-						})
-						console.debug("Unzipped")
-					}, 200)
-					// Removes the zip after unzip
-					setTimeout(() => {
-						exec("del ./depotdownloader/depotdownloader.zip", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.log(stderr)
-							}
-						})
-						console.debug("Removed leftover zip")
-					}, 350)
-					setTimeout(() => {
-						console.info("Finished extracting DepotDownloader binary..")
-						console.debug("Starting dotnet process...")
-						var finalcommand
-						finalcommand = `cmd.exe  /k dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16`
-
-						exec(finalcommand, (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-
-							}
-						})
-					}, 400)
-				} else if (os.platform().includes("linux")) {
+					// The following code has delays in between because async io operations won't work well
+					// Cleans up old files (but not the downloaded zip)
 					setTimeout(() => {
 						exec("file ./depotdownloader/* | grep -vi zip | cut -d: -f1 | tr '\\n' '\\0' | xargs -0 -r rm", (err, stdout, stderr) => {
 							if (err) {
@@ -208,77 +154,11 @@ function submitForm() {
 							}
 						})
 					}, 400)
-				} else if (os.platform().includes("macos")) {
-					setTimeout(() => {
-						exec("file ./depotdownloader/* | grep -vi zip | cut -d: -f1 | tr '\\n' '\\0' | xargs -0 -r rm", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.error(stderr)
-							}
-						})
-						console.debug("Removed leftover files")
-					}, 100)
-					// Unzips downloaded zip
-					setTimeout(() => {
-						exec("unzip -o ./depotdownloader/depotdownloader.zip -d ./depotdownloader/", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.error(stderr)
-							}
-						})
-						console.debug("Unzipped")
-					}, 200)
-					// Removes the zip after unzip
-					setTimeout(() => {
-						exec("rm ./depotdownloader/depotdownloader.zip", (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-								return
-							}
-							if (stderr !== "") {
-								console.log(stderr)
-							}
-						})
-						console.debug("Removed leftover zip")
-					}, 350)
-					setTimeout(() => {
-						console.info("Finished extracting DepotDownloader binary..")
-						console.debug("Starting dotnet process...")
-						var finalcommand
-						if (osdropdown.options[osdropdown.selectedIndex].text.includes("Gnome")) {
-							finalcommand = `gnome-terminal -e 'bash -c "dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16";bash'`
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("Windows")) {
-							finalcommand = `cmd.exe  /k dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16`
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("macOS")) {
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("Konsole")) {
-							// chad konsole
-							finalcommand = `konsole --hold -e "dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16"`
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("Xfce")) {
-							finalcommand = `xfce4-terminal -H -e "dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16"`
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("Terminator")) {
-							finalcommand = `terminator -e 'bash -c "dotnet ./depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16";bash'`
-						} else if (osdropdown.options[osdropdown.selectedIndex].text.includes("Print command")) {
-							console.log(`\COPY-PASTE THE FOLLOWING INTO YOUR TERMINAL OF CHOICE:\n\ndotnet ${__dirname}/depotdownloader/DepotDownloader.dll -username ${username} -password ${password} -app ${appid} -depot ${depotid} -manifest ${manifestid} -dir ./games/${appid}/ -max-servers 50 -max-downloads 16`)
-							finalcommand = "ls > /dev/null"
-						}
-
-						exec(finalcommand, (err, stdout, stderr) => {
-							if (err) {
-								console.error(err)
-
-							}
-						})
-					}, 400)
-				}
 
 
-			})
-		}
-	})
+				})
+			}
+		})
+
+	}
 }
